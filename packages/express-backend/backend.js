c6a9f6ae3d79f00b4b784529f0dcdb98ac2d06a8
elyssaa <eabbot02@calpoly.edu>
2025-01-27 23:02:11 -0800

    added hard deleted and seach by name/job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,17 +4,18 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-// Use express middleware to handle JSON requests
+//use express to handle json requests
 app.use(express.json());
 
-// Sample data for users
+// Sample data
 const users = {
   users_list: [
     { id: "xyz789", name: "Charlie", job: "Janitor" },
     { id: "abc123", name: "Mac", job: "Bouncer" },
     { id: "ppp222", name: "Mac", job: "Professor" },
     { id: "yat999", name: "Dee", job: "Aspiring actress" },
-    { id: "zap555", name: "Dennis", job: "Bartender" }
+    { id: "zap555", name: "Dennis", job: "Bartender" },
+    { id: "qwe123", name: "Cindy", job: "Zookeeper"}
   ]
 };
 
@@ -67,10 +68,38 @@ const addUser = (user) => {
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
-  res.status(200).send(); // Send a success response with status code 200
+  res.status(200).send(); // send a success response with status code 200
 });
 
-// Start the server
+// start the server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+//Hard delete by user id
+app.delete('/users/:id', (req, res) => {
+  const userID = parseInt(req.params.id, 10);
+  const userIndex = users.findIndex(user => user.id === userId);
+  if (userIndex !== -1) {
+    users.splice(userIndex, 1);
+    res.status(200).json({ message: `User with id ${userId} deleted.` });
+} else {
+    res.status(404).json({ message: 'User not found.' });
+}
+});
+
+// Get all users matching a given name and job
+app.get('/users/search', (req, res) => {
+  const { name, job } = req.query;
+  if (!name || !job) {
+      return res.status(400).json({ message: 'Name and job are required.' });
+  }
+
+  const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()) && user.job.toLowerCase().includes(job.toLowerCase()));
+
+  if (filteredUsers.length > 0) {
+      res.status(200).json(filteredUsers);
+  } else {
+      res.status(404).json({ message: 'No users found matching your criteria.' });
+  }
 });
